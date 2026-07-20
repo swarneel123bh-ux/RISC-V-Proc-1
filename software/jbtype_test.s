d1ec9cfe75cd6f6a6dc340c25ded07394eca2f04
swarneel123bh-ux <swarneel123bh@gmail.com>
2026-07-21 00:15:05 +0530

    Problem was addi needs 3 nops for the next instruction to work properly. Issue happened due to lack of forwarding/hazard-detection unit. Will implement that next

--- software/jbtype_test.s
+++ software/jbtype_test.s
@@ -2,6 +2,10 @@
 .globl _start
 _start:
     addi  x1, x0, 1          # x1=1  (runs; marker that we started)
+    # NEED NOPS HERE BECAUSE FORWARDING UNIT NEEDED TO RESOLVE JAL PROPERLY
+    nop
+    nop
+    nop
 
     # ---- JAL: jump forward, skip the poison, link to x5 ----
     jal   x5, jal_target
@@ -32,15 +36,25 @@ bne_wrong:
 after_bne:
     # ---- JALR: load a target addr into x12, jump through it ----
     la    x12, jalr_target   # x12 = &jalr_target  (pseudo-op; see note)
+    # NOPS NEEDED HERE BECAUSE DATA NEEDS TO BE FORWARDED (LA IS PSEUDO INSTR, EXPANDS TO ADDI)
+    nop
+    nop
+    nop
     jalr  x13, 0(x12)        # jump to x12, link x13
     addi  x15, x0, 0x7AD     # POISON: skipped -> x15 stays 0
+    addi x16, x0, 0x7AD				# POISON: skipped -> x16 stays 0 (jalr skips next two instructions, so the target need
+    # to be at least two instructions away ??)
 
 jalr_target:
     addi  x14, x0, 0xBE      # x14=0x22 proves JALR landed
 
 hang_:
 		la x25, hang
-hang:
+		# SAME REASON NOPS NEEDED HERE
+		nop
+		nop
+		nop
+hang:			 # Loop indefinitely
 		jalr x26, 0(x25)
 		addi x20, x0, 0x7AD					# POISON
 
